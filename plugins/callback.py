6f62d3e73f85ae859888364ad6b7b9173a935eff
Mukesh <100356452+Itz-mst-boy@users.noreply.github.com>
2022-03-15 21:55:22 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -53,12 +53,12 @@ async def cb_handler(bot: Client, query: CallbackQuery):
             reply_markup=InlineKeyboardMarkup(
 			[
 				[
-					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/subinps'),
-					InlineKeyboardButton("🤖Other Bots", url="https://t.me/subin_works/122"),
-                    InlineKeyboardButton("⚙️Update Channel", url="https://t.me/subin_works")
+					InlineKeyboardButton("👨🏼‍💻Developer", url='https://t.me/itz_mst_boy'),
+					InlineKeyboardButton("🤖Other Bots", url="https://t.me/mastermind_network_official/37"),
+                    InlineKeyboardButton("⚙️Update Channel", url="https://t.me/mr_sukkun")
 				],
 				[
-					InlineKeyboardButton("🔗Source Code", url="https://github.com/subinps/Instagram-Bot"),
+					InlineKeyboardButton("🔗Source Code", url="https://github.com/itz-mst-boy/Instagram-Bot"),
 					InlineKeyboardButton("🧩Deploy Own Bot", url="https://heroku.com/deploy?template=https://github.com/subinps/Instagram-Bot")
 				]
 			]
